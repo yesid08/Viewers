a1bb417b2c6eece3bc68865d186187de93bb8d63
Erik Ziegler <erik.sweed@gmail.com>
2020-02-05 13:06:44 +0100

    wip2

--- .circleci/start-test-server.sh
+++ .circleci/start-test-server.sh
@@ -15,6 +15,8 @@ curl -X PUT http://localhost:5984/_replicator
 curl -X PUT http://localhost:5984/_global_changes
 
 # Setup Python client
+add-apt-repository ppa:deadsnakes/ppa
+apt-get update
 apt-get -y install python3.6 python3-pip
 pip install pydicom
 pip install dicomweb-client
