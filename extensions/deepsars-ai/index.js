df624224055bdf294da54293a0952152e7545d0b
you@example.com <yesid.08@hotmail.com>
2020-06-18 17:28:51 -0500

    Fixed Extension project structure

--- extensions/deepsars-ai/index.js
+++ extensions/deepsars-ai/index.js
@@ -1,86 +1,10 @@
 import cornerstone from 'cornerstone-core';
 import { retrieveAllMeasurements } from './Measurements';
-
+import ToolbarModule from './ToolbarModule';
 export const DeepSARSAiExtension = {
   id: 'volumenCtExtension',
   getToolbarModule() {
-    return {
-      definitions: [
-        {
-          id: 'Volumen-CT',
-          label: 'Volumen-CT',
-          icon: 'brain',
-          buttons: [
-            {
-              id: 'PredecirVolumen',
-              label: 'Predecir',
-              icon: 'play',
-              type: 'command',
-              commandName: 'predecirVolumenCt',
-            },
-            {
-              id: 'pathologyVolumen',
-              label: 'Patología',
-              icon: 'lung',
-            },
-            {
-              id: 'precisionVolumen',
-              label: 'Precisión',
-              icon: 'measure-target',
-            },
-          ],
-        },
-        {
-          id: 'Slice-CT',
-          label: 'Slice-CT',
-          icon: 'brain',
-          buttons: [
-            {
-              id: 'PredecirSlice',
-              label: 'Predecir',
-              icon: 'play',
-              type: 'command',
-              commandName: 'predecirSliceCt',
-            },
-            {
-              id: 'pathologySlice',
-              label: 'Patología',
-              icon: 'lung',
-            },
-            {
-              id: 'precisionSlice',
-              label: 'Precisión',
-              icon: 'measure-target',
-            },
-          ],
-        },
-        {
-          id: 'Slice-RX',
-          label: 'Slice-RX',
-          icon: 'brain',
-          buttons: [
-            {
-              id: 'PredecirSlice',
-              label: 'Predecir',
-              icon: 'play',
-              type: 'command',
-              commandName: 'predecirSliceRx',
-            },
-            {
-              id: 'pathologySlice',
-              label: 'Patología',
-              icon: 'lung',
-            },
-            {
-              id: 'precisionSlice',
-              label: 'Precisión',
-              icon: 'measure-target',
-            },
-          ],
-        },
-      ],
-      defaultContext: 'VIEWER',
-    };
+    return ToolbarModule;
   },
   getCommandsModule({ servicesManager, commandsManager }) {
     const { UINotificationService } = servicesManager.services;
