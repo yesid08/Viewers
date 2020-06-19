7511e3f98a982f1409cad8e2dd3a06d1474ec591
you@example.com <yesid.08@hotmail.com>
2020-06-18 21:45:10 -0500

    Added Measurements handlers saveMeasurements and RetrieveAllMeasurements.

--- extensions/deepsars-ai/Measurements.js
+++ extensions/deepsars-ai/Measurements.js
@@ -1,28 +1,25 @@
 import OHIF from '@ohif/core';
+import cornerstone from 'cornerstone-core';
 import { tempMeasurements } from './mocks/MeasurementsMock';
+
 const localMeasurementAPI = OHIF.measurements.MeasurementApi.Instance;
-export const onSaveMeasurements = () => {
+
+export const saveMeasurements = () => {
   console.log('The following tool anotations are going to be saved');
   console.log(localMeasurementAPI.tools);
 };
 
 export const retrieveAllMeasurements = () => {
-  localMeasurementAPI = OHIF.measurements.MeasurementApi.Instance;
-  console.log(localMeasurementAPI);
-  console.log(localMeasurementAPI.tools);
   tempMeasurements.forEach(tempMeasurements =>
     localMeasurementAPI.addMeasurement(
       tempMeasurements.toolType,
       tempMeasurements
     )
   );
-
   localMeasurementAPI.syncMeasurementsAndToolData();
-
   cornerstone.getEnabledElements().forEach(enabledElement => {
     cornerstone.updateImage(enabledElement.element);
   });
-
   // Let others know that the measurements are updated
   localMeasurementAPI.onMeasurementsUpdated();
 };
