b5042796bb08e8ee7d2fd68f92b7af09e819523d
you@example.com <yesid.08@hotmail.com>
2020-06-24 15:20:19 -0500

    added DICOM UIDs utility

---
+++ extensions/deepsars-ai/utils.js
@@ -0,0 +1,11 @@
+import cornerstone from 'cornerstone-core';
+export const getdicomUIDs = () => {
+  const defaultEnabledElement = cornerstone.getEnabledElements()[0];
+  const image = defaultEnabledElement.image;
+  const imageIdArray = image.imageId.split('/');
+  return {
+    StudyInstanceUID: imageIdArray.slice(-7)[0],
+    SeriesInstanceUID: imageIdArray.slice(-5)[0],
+    SOPInstanceUID: imageIdArray.slice(-3)[0],
+  };
+};
