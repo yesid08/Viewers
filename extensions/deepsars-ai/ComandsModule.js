759771c9556295258f3c2e1fc10cfe736dfa2048
you@example.com <yesid.08@hotmail.com>
2020-07-08 11:39:58 -0500

    Added todo list (code refactoring)

--- extensions/deepsars-ai/ComandsModule.js
+++ extensions/deepsars-ai/ComandsModule.js
@@ -1,5 +1,13 @@
 import { getDicomUIDs, getAllInstancesUIDs } from './utils';
 
+/*TODO List: By Yesid G.
+  1) Create petitions utilities inside of utils.py
+  2) Create the logic of each button in the corresponding section (heatmaps.js or predictions.js) by using functions.
+  3) Inside this file(commands module) just call the functions from heatmaps.js or predictions.js in front of each commandFn.
+  4) This file should have the same form of the one that is currently in index, the idea is to export the whole object to index.
+  5) In the file index.js import this commandsModule.js file and return the object of this file
+  6) commit the changes and have fun!
+*/
 export const launchPetition = (petition, url, requestPayload) => {
   const promisePetition = new Promise((resolve, reject) => {
     var xhttp = new XMLHttpRequest();
--- extensions/deepsars-ai/index.js
+++ extensions/deepsars-ai/index.js
@@ -7,9 +7,18 @@ import {
 } from './ComandsModule';
 import { getDicomUIDs, getAllInstancesUIDs } from './utils';
 import cornerstoneTools from 'cornerstone-tools';
-
 import ToolbarModule from './ToolbarModule';
 import * as BUTTONS from './ToolbarModule';
+
+/*TODO List: By Yesid G.
+  1) Create petitions utilities inside of utils.py
+  2) Create the logic of each button in the corresponding section (heatmaps.js or predictions.js) by using functions.
+  3) Inside this file(commands module) just call the functions from heatmaps.js or predictions.js in front of each commandFn.
+  4) This file should have the same form of the one that is currently in index, the idea is to export the whole object to index.
+  5) In the file index.js import this commandsModule.js file and return the object of this file
+  6) commit the changes and have fun!
+*/
+
 export const DeepSARSAiExtension = {
   id: 'volumenCtExtension',
   getToolbarModule() {
@@ -35,7 +44,7 @@ export const DeepSARSAiExtension = {
 
             var requestPromise = launchPetition(
               'POST',
-              'http://localhost/trs/aiModels/',
+              '/trs/aiModels/',
               requestPayload
             );
             requestPromise
