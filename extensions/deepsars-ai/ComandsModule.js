96c387e4ec95fad85bd168581cc869a837db2903
Brayan Valenzuela <b.rayancamilo97@hotmail.com>
2020-07-09 11:25:00 -0500

    map

--- extensions/deepsars-ai/ComandsModule.js
+++ extensions/deepsars-ai/ComandsModule.js
@@ -9,10 +9,11 @@
 */
 
 import { retrieveAllMeasurements, saveMeasurements } from './Measurements';
-import { getDicomUIDs, getAllInstancesUIDs } from './utils';
+import { getDicomUIDs } from './utils';
 import cornerstoneTools from 'cornerstone-tools';
 import * as BUTTONS from './ToolbarModule';
 import * as predictions from './operationsAI/predictions';
+import * as heatmaps from './operationsAI/heatmaps';
 
 const deepsarsCommandsModule = ({ servicesManager, commandsManager }) => {
   const { UINotificationService } = servicesManager.services;
@@ -76,69 +77,15 @@ const deepsarsCommandsModule = ({ servicesManager, commandsManager }) => {
         options: {},
       },
       reload: {
-        commandFn: function() {
+        commandFn: () => {
           location.reload();
         },
         storeContexts: [],
         options: {},
       },
       load_heat_map: {
-        commandFn: function() {
-          const allDicomUIDs = getAllInstancesUIDs();
-          const targetUIDs = allDicomUIDs.filter(dicomUID => {
-            return (
-              dicomUID.SeriesInstanceUID == getDicomUIDs().SeriesInstanceUID
-            );
-          });
-
-          function loop(targetUIDs, i) {
-            if (i >= targetUIDs.length) {
-              return;
-            }
-
-            var requestPrediction = {
-              microservice: 'orthanc',
-              task: 'prepare',
-              file_ID: targetUIDs[i].SOPInstanceUID,
-              file_type: 'slice',
-              file_mod: 'ct',
-              file_view: 'axial',
-            };
-
-            var requestPayload = {
-              count: 0,
-              data: requestPrediction,
-              date: String(new Date().getTime()),
-              enterprise: 1,
-              ips: ['2QEJ8cO8CC'],
-              operation: 'read',
-              route: 'aiModels',
-              _v: 3,
-            };
-            UINotificationService.show({
-              title: 'Realizando predicción',
-              message: 'Este proceso tomara unos segundos.',
-            });
-
-            var url = 'https://deepsars.uis.edu.co/trs/aiModels/';
-            var request = new XMLHttpRequest();
-            request.open('POST', url);
-            request.onreadystatechange = function() {
-              if (
-                request.readyState === XMLHttpRequest.DONE &&
-                request.status === 200
-              ) {
-                loop(targetUIDs, i + 1);
-              }
-            };
-            request.setRequestHeader(
-              'Authorization',
-              'eW1I0vBmaURGlp0uo9a7gm3cQ8EpRgcQYyIa86uXIIFVEbWrrwnnfVgE8xtR22LkfIpXUPrM4h9FQwhdvqqioUPKHycVPNrSQ7ssV40JNUHmw35Jeks0Q8zI+ad8vaLD4tlhxQ87h7t57mNGP7Utgni37geebJ6lHzqwubrYgPfBjvu/ODLO/HItbC4/r4iT4sFG6r0ZjFWM76Gm8mk9+Qr/tFCIIszfpIecc34pinRoEoCmIZNjqJjEkjS5IiYQeNYNJmt4a1RESfLZ5Xf5LZw3pA5NhRisRGpoLTDqJdPUzo0qKMo3INpivZxjm93XOiJ5wKpQEgwblPQ2Qd+lbWp3aujQzEXWVzcK1AGZKGtzhnjOw+eAJEcEO4ygRntUDNIqv5wAlaLvr7d2UoLcbq42ZCKC/EcBXPhJ6MGBkYCi1Wh6zG5kXfuJeSaFvjzHHYDvstN39K6vyUbFJa+gow/pw1vSaLLellv1lQ/WPy4G1+JFQpeVC9iOGFaRKAaH7IaUfZLrjwSsMzF9hyZ53cO92mttVjNAwLx30fK4LfMRUzLGJwEk7nzxm+zElptqfSru/IIVqPWuBZdzGYuFK6JOXYLEf+1ULzXF5SShLCcMKT9hcIRB9FRzIELgEvUMTgKgW4s5AxQJckMUbkgTXziXWiT/iL49a03eLvc1i8Lm4UIWLTWU/+bm0VnHo+b4C5kYHIyyKkToThwTAWj8C5tmgWE1U5/rJrqXndt3fiVdQtk/fCHxD9BsM2gM/osF92KjmZ3axJLn3Zm0JTOVjBxsTN1Sz1S8nIHvIs/CVmJfp1UFz8FfETWwzJYjQKguOAW7xnPfypZGY544soXmGlo9fzFT+5b3uZBYPrf2al4cOUDNz/w2MtWdPSQeNgRmYS5oFLBUTUb7wsHqA3QJgTEK2Rkuy0ZSpzSlUzqtM33jPMz8uqAUVmGjk6kE+vixHpHwdT8drLcoEyvoaHPTfV1j1pmuGCJq2Bqk0qGpcHXH/4cp3yNockBwFEWXhGnp6A=='
-            );
-            request.setRequestHeader('Content-Type', 'application/json');
-            request.send(JSON.stringify(requestPayload));
-          }
-          loop(targetUIDs, 0);
+        commandFn: () => {
+          heatmaps.heatMaps(UINotificationService);
         },
         storeContexts: [],
         options: {},
--- extensions/deepsars-ai/operationsAI/heatmaps.js
+++ extensions/deepsars-ai/operationsAI/heatmaps.js
@@ -6,3 +6,46 @@
   5) In the file index.js import this commandsModule.js file and return the object of this file
   6) commit the changes and have fun!
 */
+import * as utils from '../utils';
+
+export const heatMaps = UINotificationService => {
+  var dicomData = utils.getDicomUIDs();
+  var dicomDataArray = Object.values(dicomData);
+  var studyData = {
+    microservice: 'models',
+    file_mod: 'CT',
+    file_view: 'axial',
+    task: 'prepare',
+    file_type: 'slice',
+    file_ID: dicomDataArray[1],
+  };
+  var promisePetition = utils.makeTransaction('aiModels', 'read', studyData);
+
+  UINotificationService.show({
+    title: 'Generando mapas de calor',
+    message: 'Este proceso tomara unos segundos.',
+  });
+
+  promisePetition
+    .then(response => {
+      if (response.data.hasOwnProperty('error')) {
+        UINotificationService.show({
+          title: 'Error de Predicción',
+          type: 'warning',
+          message: 'Por favor intente de nuevo',
+        });
+      } else {
+        UINotificationService.show({
+          title: 'Mapas de calor generados',
+          message: 'Para visualizarlos por favor recargue la página',
+        });
+      }
+    })
+    .catch(rst => {
+      UINotificationService.show({
+        type: 'error',
+        title: 'Error',
+        message: 'Sin conexión.',
+      });
+    });
+};
