b3d1c173eaf041a80d3af5e968d8ae6ff7e73ff6
you@example.com <yesid.08@hotmail.com>
2020-07-18 00:13:36 -0500

    Fixed lesionNamingNumber and measurementNumber

--- extensions/deepsars-ai/Measurements.js
+++ extensions/deepsars-ai/Measurements.js
@@ -1,43 +1,49 @@
 import OHIF from '@ohif/core';
 import cornerstone from 'cornerstone-core';
-import { tempMeasurements } from './mocks/MeasurementsMock';
-import studyMetadataManager from '../../platform/core/src/utils/studyMetadataManager.js';
-import { makeTransaction } from './utils'
+import { makeTransaction } from './utils';
 
 export const saveMeasurements = () => {
   const localMeasurementAPI = OHIF.measurements.MeasurementApi.Instance;
-  const currentMeasurements = localMeasurementAPI.tools.RectangleRoi.concat(localMeasurementAPI.tools.EllipticalRoi);
-  console.log(`${currentMeasurements.length} tool anotations are going to be saved`);
+  const currentMeasurements = localMeasurementAPI.tools.RectangleRoi.concat(
+    localMeasurementAPI.tools.EllipticalRoi
+  );
+  console.log(
+    `${currentMeasurements.length} tool anotations are going to be saved`
+  );
 
   if (currentMeasurements.length !== 0) {
-    currentMeasurements.forEach((measurement) => {
+    currentMeasurements.forEach(measurement => {
       delete measurement._id;
       makeTransaction('roiAnnotations', 'write', measurement)
-        .then((response) => {
+        .then(response => {
           console.log('Respuesta=>', response);
         })
-        .catch((error) => {
+        .catch(error => {
           console.log('Error=>', error);
         });
     });
   }
-
 };
 
-export const retrieveAllMeasurements = (study) => {
+export const retrieveAllMeasurements = study => {
   const localMeasurementAPI = OHIF.measurements.MeasurementApi.Instance;
-  const currentMeasurements = localMeasurementAPI.tools.RectangleRoi.concat(localMeasurementAPI.tools.EllipticalRoi);
-  console.log("Sacando anotaciones del estudio =>", study);
-  console.log("ANOTACIONES ACTUALES", currentMeasurements);
+  const currentMeasurements = localMeasurementAPI.tools.RectangleRoi.concat(
+    localMeasurementAPI.tools.EllipticalRoi
+  );
+  console.log('Sacando anotaciones del estudio =>', study);
+  console.log('ANOTACIONES ACTUALES', currentMeasurements);
   makeTransaction('roiAnnotations', 'readList', { StudyInstanceUID: study })
-    .then((response) => {
+    .then(response => {
       console.log('Respuesta=>', response.data);
       response.data.forEach(measurement => {
-        if (!currentMeasurements.some((e) => { return (e._roiId === measurement._roiId) })) {
-          localMeasurementAPI.addMeasurement(
-            measurement.toolType,
-            measurement
-          )
+        if (
+          !currentMeasurements.some(e => {
+            return e._roiId === measurement._roiId;
+          })
+        ) {
+          measurement.lesionNamingNumber = currentMeasurements.length + 1;
+          measurement.measurementNumber = currentMeasurements.length + 1;
+          localMeasurementAPI.addMeasurement(measurement.toolType, measurement);
         }
       });
       console.log(OHIF.measurements.MeasurementApi.Instance);
@@ -49,7 +55,7 @@ export const retrieveAllMeasurements = (study) => {
       // Let others know that the measurements are updated
       localMeasurementAPI.onMeasurementsUpdated();
     })
-    .catch((error) => {
+    .catch(error => {
       console.log('Error=>', error);
     });
 };
