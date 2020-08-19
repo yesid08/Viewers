d6e4d4b777703c7c37624d07cbba05c2ba0193ce
you@example.com <yesid.08@hotmail.com>
2020-08-18 19:59:38 -0500

    added ify-loader

--- .webpack/webpack.base.js
+++ .webpack/webpack.base.js
@@ -54,6 +54,7 @@ module.exports = (env, argv, { SRC_DIR, DIST_DIR }) => {
         transpileJavaScriptRule(mode),
         loadWebWorkersRule,
         loadShadersRule,
+        { test: '/.js$/', loader: 'ify-loader' },
       ],
     },
     resolve: {
@@ -81,8 +82,12 @@ module.exports = (env, argv, { SRC_DIR, DIST_DIR }) => {
         'process.env.BUILD_NUM': JSON.stringify(BUILD_NUM),
         /* i18n */
         'process.env.USE_LOCIZE': JSON.stringify(process.env.USE_LOCIZE || ''),
-        'process.env.LOCIZE_PROJECTID': JSON.stringify(process.env.LOCIZE_PROJECTID || ''),
-        'process.env.LOCIZE_API_KEY': JSON.stringify(process.env.LOCIZE_API_KEY || ''),
+        'process.env.LOCIZE_PROJECTID': JSON.stringify(
+          process.env.LOCIZE_PROJECTID || ''
+        ),
+        'process.env.LOCIZE_API_KEY': JSON.stringify(
+          process.env.LOCIZE_API_KEY || ''
+        ),
       }),
     ],
     // Fix: https://github.com/webpack-contrib/css-loader/issues/447#issuecomment-285598881
