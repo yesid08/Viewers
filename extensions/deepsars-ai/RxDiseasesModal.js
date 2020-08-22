cc341e0be32378703cbcb158b89cd235c8846fb7
you@example.com <yesid.08@hotmail.com>
2020-08-22 14:31:12 -0500

    added Modal for distribution

--- extensions/deepsars-ai/RxDiseasesModal.js
+++ extensions/deepsars-ai/RxDiseasesModal.js
@@ -2,15 +2,16 @@ import React from 'react';
 import Plot from 'react-plotly.js';
 import PropTypes from 'prop-types';
 
-const RxDiseasesModal = ({ chartData }) => {
+const RxDiseasesModal = ({ chartData, chartLayout }) => {
   return (
     <div>
-      <Plot data={chartData} layout={{ title: 'OHIF PLOT' }} />
+      <Plot data={chartData} layout={chartLayout} />
     </div>
   );
 };
 
 RxDiseasesModal.propTypes = {
   chartData: PropTypes.array,
+  chartLayout: PropTypes.object,
 };
 export default RxDiseasesModal;
