d5c2c38639b794a9fccab0cee0bdb101839553fd
Brayan01 <b.rayancamilo97@hotmail.com>
2020-09-22 16:16:02 -0500

    /orthanc/

--- platform/viewer/public/config/default.js
+++ platform/viewer/public/config/default.js
@@ -29,9 +29,9 @@ window.config = {
     dicomWeb: [
       {
         name: 'Orthanc',
-        wadoUriRoot: 'https://deepsars.uis.edu.co/orthanc/wado',
-        qidoRoot: 'https://deepsars.uis.edu.co/orthanc/dicom-web',
-        wadoRoot: 'https://deepsars.uis.edu.co/orthanc/dicom-web',
+        wadoUriRoot: '/orthanc/wado',
+        qidoRoot: '/orthanc/dicom-web',
+        wadoRoot: '/orthanc/dicom-web',
         qidoSupportsIncludeField: false,
         imageRendering: 'wadors',
         thumbnailRendering: 'wadors',
