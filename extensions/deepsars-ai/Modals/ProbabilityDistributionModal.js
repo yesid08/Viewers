5c6cd2d8f820ac043c488fe411fad613621b5aab
brayan01 <b.rayancamilo97@hotmail.com>
2020-11-07 11:28:10 -0500

    visual models results

--- extensions/deepsars-ai/Modals/ProbabilityDistributionModal.js
+++ extensions/deepsars-ai/Modals/ProbabilityDistributionModal.js
@@ -4,30 +4,45 @@ import PropTypes from 'prop-types';
 import './ProbabilityDistributionModalStyles.styl'
 
 const probabilityDistributionModal = ({ chartData, chartLayout, predictedClass, probability, trainingSamples, validationAcc }) => {
+  console.log(chartData, chartLayout);
+  var yValue = chartData[0].y;
+  var xValue = chartData[0].x;
+  console.log(yValue, xValue.indexOf(predictedClass), xValue.length);
+  chartData[0].text = yValue.map(String);
+  chartData[0].textposition = 'auto';
+  chartData[0].textfont = {
+    "size": 22
+  };
+
+  chartLayout.yaxis.range = [0, 100];
+  chartData[0].hoverinfo = 'none';
+
+  var colors = new Array(xValue.length);
+  for (var i = 0; i < xValue.length; i++) {
+    if (i == xValue.indexOf(predictedClass)) {
+      colors[i] = 'rgba(222,45,38,0.8)';
+    }
+    else {
+      colors[i] = 'rgba(124,179,66,0.8)';
+    }
+  }
+  console.log(colors);
+  chartData[0].marker.color = colors;
   return (
     <div>
       <div className='title'>
         {
-          '-Modelo entrenado con  '.concat(trainingSamples, " muestras.")
+          '-La clase predicha por el modelo fue '.concat("'", predictedClass, "'", ' con una confianza de ', probability, "%.")
         }
-        <div>
-          {
-            '-Modelo evaluado con una precisión del '.concat(validationAcc, "%.")
-          }
-        </div>
-        <div>
-          {
-            '-La clase predicha por el modelo fue '.concat("'", predictedClass, "'", ' con una confianza de ', probability, "%.")
-          }
-        </div>
       </div>
-      {/* <div className='title'>
-        {
-          'La clase predicha por el modelo fue '.concat("'", predictedClass, "'", ' con una confianza de ', probability, "%")
-        }
-      </div> */}
       <Plot data={chartData} layout={chartLayout} style={{ width: '100%' }} />
+      <div className='footer'>
+        {
+          '-Modelo entrenado con  '.concat(trainingSamples, " muestras y exactitud del ", validationAcc, "%.")
+        }
+      </div>
     </div>
+
   );
 };
 
